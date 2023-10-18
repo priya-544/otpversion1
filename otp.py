7f2238e186a0728cefc67e159e53862397ba64e0
Priya Tare <priyatare1354@gmail.com>
2023-10-18 23:50:07 +0530

    validate and send over E-mail

--- otp.py
+++ otp.py
@@ -15,11 +15,53 @@ def generate_otp():
 
 fotp=generate_otp()
 
+# function for checking mobile number
+def validate_mobile(num):
+    return len(num) == 10 and num.isdigit()
+
+
+# function to send otp via sms ans validate number
+def send_otp_over_mobile(target_no,fotp):
+    
+    if(validate_mobile(target_no)):
+        target_no="+91"+target_no
+        message=client.messages.create(
+          body="Your OTP is "+fotp,
+          from_=sms.twilio_no,
+          to=sms.target_no
+        )
+        print(message.body)
+      
+    else:
+       print("INVALID MOBILE NUMBER!")
+       target_no=input("ENTER AGAIN: ")
+       validate_mobile(target_no)
+       send_otp_over_mobile(target_no,fotp)
 message = client.messages.create(
     body="Your 6 digit OTP is "+otp,
     from_=sms.inputNO,
     to=sms.sendNo
 )
 
-print(message.body)
+def validate_email(mail):
+   if "@" not in mail and "." not in mail:
+      return False
+   else:
+      return True
+
+# function to send otp via email and validate email
+def send_otp_over_email(mail,fotp,sender,password):
+   if(validate_email(mail)):
+        server=smtplib.SMTP('smtp.gmail.com',587)
+        server.starttls()
+        server.login(sender,password)
+        msg_body="Your OTP is "+fotp
+        server.sendmail(sender,mail,msg_body)
+        print("OTP sent! ",fotp)
+   else:
+        print("INVALID MAIL!")
+        mail=input("ENTER AGAIN: ")
+        validate_email(mail)
+        send_otp_over_email(mail,fotp,sender,password)
+      
 
